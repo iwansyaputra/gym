5ca74a174b928492fdc6abc69b06158f3a28a952
Iwan Syaputra <iwansyaputra031204@gmail.com>
2026-05-22 17:57:56 +0700

    feat: add renderSidebar component for admin navigation menu

--- admin/js/components.js
+++ admin/js/components.js
@@ -27,8 +27,8 @@ function renderSidebar() {
 
   const sidebarHtml = `
   <div class="sidebar-brand">
-    <div class="brand-logo">
-      <svg viewBox="0 0 24 24" fill="none"><path d="M20.57 14.86L22 13.43 20.57 12 17 15.57 8.43 7 12 3.43 10.57 2 9.14 3.43 7.71 2 5.57 4.14 4.14 2.71 2.71 4.14 4.14 5.57 2 7.71 3.43 9.14 2 10.57 3.43 12 7 8.43 15.57 17 12 20.57 13.43 22 14.86 20.57 16.29 22 18.43 19.86 19.86 21.29 21.29 19.86 19.86 18.43 22 16.29 20.57 14.86Z" fill="currentColor"/></svg>
+    <div class="brand-logo" style="background: transparent; box-shadow: none;">
+      <img src="img/logo.png" alt="GymKu" style="width: 100%; height: 100%; object-fit: contain;" />
     </div>
     <div><div class="brand-name">GymKu</div><div class="brand-tag">Admin Panel</div></div>
   </div>
